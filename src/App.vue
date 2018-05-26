5e390bf5ca2311481552689a3bbc9237ca289fdb
Enamul <enamul.promy@gmail.com>
2018-05-26 21:18:32 +0600

    init commit

---
+++ src/App.vue
@@ -0,0 +1,62 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <TodoList v-bind:todos="todos"/>
+    <CreateTodo v-on:create-todo="createTodo" />
+  </div>
+</template>
+
+<script>
+import TodoList from './components/TodoList';
+import CreateTodo from './components/CreateTodo';
+
+export default {
+  name: 'App',
+  components: {
+    TodoList,
+    CreateTodo,
+  },
+  data() {
+    return {
+      todos: [{
+        title: 'Todo A',
+        project: 'Project A',
+        done: false,
+      }, {
+        title: 'Todo B',
+        project: 'Project B',
+        done: true,
+      }, {
+        title: 'Todo C',
+        project: 'Project C',
+        done: false,
+      }, {
+        title: 'Todo D',
+        project: 'Project D',
+        done: false,
+      }],
+    };
+  },
+  methods: {
+      createTodo(title) {
+        console.log(title);
+        console.log('i was here');
+      this.todos.push({
+        title,
+        done: false,
+      });
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
